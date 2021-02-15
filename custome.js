26175b570d7edff7a944f8ef044e5bb5441dcbd8
kartikekakkar <kartike.kakkar77@gmail.com>
2021-02-15 23:03:42 +0530

    first commit

---
+++ custome.js
@@ -0,0 +1,7 @@
+let ani =new Typed("#newid",{
+    strings: ["WEB DESIGNERS","UI/UX DESIGNERS","WEB DEVELOPER","LOGO DESIGNERS","RE-SEARCHERS"],
+typeSpeed:100,
+loop:true,
+backSpeed:10,
+showCursor:false,
+})
